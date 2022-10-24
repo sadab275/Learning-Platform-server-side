1bebd4699883800a8b2e00540948d49de3be1764
Sadab <sadabrahman275@gmail.com>
2022-10-25 01:57:30 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+
+
+const categories = require('./data/categories.json');
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Courses Api Running');
+});
+app.get('/courses-categories', (req, res) => {
+    res.send(categories);
+});
+
+app.listen(port, () => {
+    console.log('Programming Courses Server running on port', port);
+})
