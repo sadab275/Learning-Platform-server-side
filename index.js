235ee78bab7df650333e17750f5d39f2e1300eb4
Sadab <sadabrahman275@gmail.com>
2022-10-25 03:12:13 +0600

    categories data added

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
-
+app.use(cors());
 
 
 const categories = require('./data/categories.json');
 
-app.use(cors());
+
 
 app.get('/', (req, res) => {
     res.send('Courses Api Running');
